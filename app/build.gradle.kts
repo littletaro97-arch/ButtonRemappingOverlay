65d42f75b43b6ca52825c45549bb91d1bf2f135a
LittleTaro <littletaro97@gmail.com>
2026-09-11 23:22:48 +0800

    release: bump version to v6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.buttonremapping"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "6.9"
+        versionCode = 43
+        versionName = "6.10"
     }
 
     compileOptions {
